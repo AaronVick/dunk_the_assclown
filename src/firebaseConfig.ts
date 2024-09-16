fb63d54e4d4a34f1f148fb0db55ed96f31a7f6ea
Aaron <33428648+AaronVick@users.noreply.github.com>
2024-09-16 10:25:49 -0400

    update

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,17 +1,22 @@
-import { initializeApp, cert } from 'firebase-admin/app';
-import { getDatabase } from 'firebase-admin/database';
+import * as admin from 'firebase-admin';
 
 const firebaseConfig = {
-  credential: cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')
-  }),
-  databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
+  type: process.env.FIREBASE_TYPE,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'), // Ensure newline characters are handled
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  clientId: process.env.FIREBASE_CLIENT_ID,
+  authUri: process.env.FIREBASE_AUTH_URI,
+  tokenUri: process.env.FIREBASE_TOKEN_URI,
+  authProviderCertUrl: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  clientCertUrl: process.env.FIREBASE_CLIENT_CERT_URL
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(firebaseConfig),
+  });
+}
 
-// Export the database instance
-export const db = getDatabase(app);
+export default admin;
